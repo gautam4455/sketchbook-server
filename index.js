1136168af5511121bb76b429582acf7a400bdaa5
gautam <gautam.kamble4455@gmail.com>
2025-01-26 20:21:06 +0530

    Added Draw and Config Events.

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ const httpServer = createServer(app);
 const io = new Server(httpServer, { cors: 'http://localhost:3000' });
 
 io.on("connection", (socket) => {
-  console.log('server connected');
+  console.log('server connected'); // if server is connected or not
 
+  // To draw a line
+  socket.on('beginPath', (arg) => {
+    socket.broadcast.emit('beginPath', arg)
+  });
+  socket.on('drawLine', (arg) => {
+    socket.broadcast.emit('drawLine', arg)
+  });
+
+  // To keep the color and size config same
+  socket.on('changeConfig', (arg) => {
+    socket.broadcast.emit('changeConfig', arg)
+  });
 });
 
 httpServer.listen(4000);
